874f45ae846c6f984eecb5bef09376cb62e25195
Sheena Walsh <115580397+Sheenor101@users.noreply.github.com>
2023-11-26 18:55:47 +0000

    Mixamo Animations and Moving Food

---
+++ Assets/Scripts/RotateScript1.cs
@@ -0,0 +1,24 @@
+/// this code was copied from a Udemy course. https://www.udemy.com/course/develop-augmented-reality-book-ar-business-card-with-unity/learn/lecture/8516356#overview
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+
+{
+    public int ydirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0,ydirection,0) * Time.deltaTime);
+        
+    }
+}
