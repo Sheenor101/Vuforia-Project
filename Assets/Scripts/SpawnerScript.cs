4a93b8dc7ac99f253fbe693bfaa1044d2be02117
Sheena Walsh <115580397+Sheenor101@users.noreply.github.com>
2023-12-18 10:08:45 +0000

    Trying to get objects to spawn
    My Unity crashed a couple of times, so I'll be committing  more frequently from now on just in case...

--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -3,18 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnerScript : MonoBehaviour
-{
-   public GameObject foodPrefab;
 
-//i want to change the below code to an automatic spawn, rather than one that is triggered by a key press
+public class SpawnerScript : MonoBehaviour
+
+
+//i want to change my code to an automatic spawn, rather than one that is triggered by a key press.
+// I was getting an error saying "'Instantiate' does not exist in the current context" so I went to the Unity forums 
+// and found this code (https://forum.unity.com/threads/instantiate-does-not-exist-in-the-current-context.48207/)
+// I am still not happy with this as I dont want the player to have to press a button to spawn the object, I want it to spawn automatically.
+  
+  // Instantiate a rigidbody then set the velocity
+{
+    // Assign a Rigidbody component in the inspector to instantiate
+
+    public Rigidbody projectile;
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Ctrl was pressed, launch a projectile
+        if (Input.GetButtonDown("Fire1"))
         {
-            instantiate(foodPrefab, transform.position, Quaternion.identity);
+            // Instantiate the projectile at the position and rotation of this transform
+            Rigidbody clone;
+            clone = Instantiate(projectile, transform.position, transform.rotation);
+
+            // Give the cloned object an initial velocity along the current
+            // object's Z axis
+            clone.velocity = transform.TransformDirection(Vector3.forward * 10);
         }
     }
-     
-    
 }
