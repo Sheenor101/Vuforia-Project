f1e70ff0659fe30c88a7da72019316324179ebe6
Sheena Walsh <115580397+Sheenor101@users.noreply.github.com>
2023-12-19 10:32:53 +0000

    Trying to Spawn enemy food again

--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -8,27 +8,46 @@ using Vuforia;
 public class SpawnerScript : MonoBehaviour
 
 
-//I went back to this code as the other was not working. CoPilot filled this in for me so I thought I would save and see what happened. 
-// I then started messing around with the code to try and get the objects to move int he direction I want. Pure trial and error
-
+//With a good nights sleep I decied to go back to figuring out how to spawn the objects. I looked at this youtube tutorial
+//https://www.youtube.com/watch?v=EVZQJeGzs0U and added some code from that. 
 {
-    public GameObject obj;
-
-    public float spawnMin = 1f;
-    public float spawnMax = 2f;
+    public GameObject item;
+    public float delay = 2f;
+    public float speed = 2f;
+    float nextTimeToSpawn;
 
     // Start is called before the first frame update
     void Start()
     {
-        Spawn();
+        nextTimeToSpawn = Time.time;
         
     }
 
-    void Spawn()
+    void Update()
     {
-        Instantiate(obj, transform.position, Quaternion.identity);
-        Invoke("Spawn", Random.Range(spawnMin, spawnMax));
-       
+        if (Time.time > nextTimeToSpawn)
+        {
+          nextTimeToSpawn = Time.time + delay;
+          GameObject go = Instantiate(item, new Vector3(Random.Range(-5f, 5f), 0, Random.Range(-5f, 5f)), Quaternion.identity);
+          go.AddComponent<Move>();
+          go.AddComponent<Move>().speed = speed; 
     }
+    //I'm using this tutorial to see if I can spawn objects randomly https://www.youtube.com/watch?v=IbiwNnOv5So
+    // its not working so i disabled it
+    // {Vector3 randomSpawnPos = new Vector3(Random.Range(-5f, 5f), 0, Random.Range(-5f, 5f));}
 }
-  
+}
+public class Move : MonoBehaviour
+{
+    public float speed = 3f;
+    void Update()
+
+   
+    {
+       transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+    //void OnBecomeInvisible()
+    //{
+    //    Destroy(gameObject);
+    //}
+}
